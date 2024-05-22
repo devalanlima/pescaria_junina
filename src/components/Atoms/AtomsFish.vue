1b593b7d0a19fe91453e182350eaba03be97bdc4
Alan Lima <dev.alanlima@gmail.com>
2024-05-22 11:11:26 -0300

    feat: Create Fish component

---
+++ src/components/Atoms/AtomsFish.vue
@@ -0,0 +1,34 @@
+<template>
+  <img :src="atualFishType" :alt="props.fishType">
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  fishType: FishColor;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  fishType: 'laranja',
+})
+
+const atualFishType = computed(()=>{
+  switch (props.fishType) {
+    
+    case 'laranja':
+      return '/peixelaranja.png'
+
+    case 'rosa':
+    return '/peixerosa.png'
+
+    default:
+      return '/peixerosa.png'
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
