036267cb20d1762a2496be2f5cb809444a5a88e8
Alan Lima <dev.alanlima@gmail.com>
2024-05-22 11:17:06 -0300

    feat: Create fish burried component

---
+++ src/components/Molecules/MoleculesFishBuried.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="relative grid place-items-center">
+      <AtomsSandPile
+      class="absolute -bottom-2"
+      :class="!props.isBurried && 'hidden'"
+      />
+      <AtomsFish :fish-type="props.fishType" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AtomsFish from '../Atoms/AtomsFish.vue';
+import AtomsSandPile from '../Atoms/AtomsSandPile.vue';
+
+interface Props {
+  isBurried: boolean;
+  fishType: FishColor;
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<style scoped>
+
+</style>
