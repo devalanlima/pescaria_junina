f07187cfb31c6fb6d6536608c92431adfe138fef
Alan Lima <dev.alanlima@gmail.com>
2024-06-04 12:28:48 -0300

    feat: Create Frog fishing component

---
+++ src/components/Molecules/MoleculesFrogFishing.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="relative" ref="container">
+        <AtomsRope class="absolute -left-[485px] -top-[40px]"/>      
+        <AtomsFrog class="w-[900px]"/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AtomsFrog from '../Atoms/AtomsFrog.vue';
+// @ts-ignore
+import AtomsRope from './MoleculesRope.vue';
+import { ref, onMounted} from "vue";
+
+const mouseX = ref(0)
+const mouseY = ref(0)
+
+const onMouseMove = (x: number, y: number) => {
+  mouseX.value = x
+  mouseY.value = y
+};
+
+const container = ref<HTMLElement | null>(null)
+
+onMounted(()=>{
+  container.value?.addEventListener('mousemove', (event) => {
+    const rect = container.value?.getBoundingClientRect();
+    if (rect !== undefined) {
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      onMouseMove(x, y);
+    }
+  });
+})
+
+</script>
+
+<style scoped>
+
+</style>
