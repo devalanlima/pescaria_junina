319234fe67600613a3b53a224a6d67d71bd7c017
Alan Lima <dev.alanlima@gmail.com>
2024-06-04 12:26:07 -0300

    refactor: Pass new props from AtomsFish

--- src/components/Molecules/MoleculesFishBuried.vue
+++ src/components/Molecules/MoleculesFishBuried.vue
@@ -1,22 +1,22 @@
 <template>
-  <div>
-    <div class="relative grid place-items-center">
-      <AtomsSandPile
-      class="absolute -bottom-2"
-      :class="!props.isBurried && 'hidden'"
-      />
-      <AtomsFish :fish-type="props.fishType" />
+    <div>
+      <div class="relative w-fit h-fit grid place-items-center">
+        <AtomsSandPile
+        class="absolute bottom-8 z-10"
+        :class="!props.isBurried && 'hidden'"
+        />
+        <AtomsFish :fish-size="props.fishSize" :color="props.color" :is-revealed="props.isRevealed"  />
+      </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
 import AtomsFish from '../Atoms/AtomsFish.vue';
 import AtomsSandPile from '../Atoms/AtomsSandPile.vue';
+import type { Fish } from '@/types/PescariaTypes';
 
-interface Props {
+interface Props extends Fish {
   isBurried: boolean;
-  fishType: FishColor;
 }
 
 const props = defineProps<Props>();
