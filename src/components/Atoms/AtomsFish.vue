6ab851c2ef6cc8f60b74f7e00c005b64a9474853
Alan Lima <dev.alanlima@gmail.com>
2024-06-04 11:49:47 -0300

    refactor: Implement flip and new props

--- src/components/Atoms/AtomsFish.vue
+++ src/components/Atoms/AtomsFish.vue
@@ -1,34 +1,139 @@
 <template>
-  <img :src="atualFishType" :alt="props.fishType">
+  <div
+  class="transition-all duration-1000 ease-out"
+  :class="atualSize.fishSize"
+  >
+    <!-- Image Container -->
+    <div :class="props.isRevealed ? 'h-[130px]' : 'h-[130px] overflow-hidden'">
+      <figure
+        ref="flipContainer"
+        @mouseleave="resetRotation"
+        tabindex="0"
+        class="flip-container relative cursor-pointer rounded-3xl h-[130px]"
+      
+      >
+        <!-- Front Side Of Card -->
+        <img class="flip-card-front transition-all duration-1000 ease-in-out absolute left-0 top-0" :src="atualFishType" :alt="'Peixe' + props.color">
+        <!-- Back Side Of Card -->
+        <div class="flip-card-back transition-all duration-1000 ease-in-out absolute left-0 top-0 grid place-items-center">
+            <p
+            class="absolute bg-white text-black rounded-full grid place-items-center bg-opacity-50"
+            :class="atualSize.numberSize"
+            >
+              1
+            </p>
+          <img class="w-full h-full"  :src="atualFishType" :alt="'Peixe' + props.color">
+        </div>
+      </figure>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref, watch } from 'vue';
+import type { Fish, FishColor } from '@/types/PescariaTypes';
 
-interface Props {
-  fishType: FishColor;
-}
+const atualSize = computed<{
+  fishSize: string;
+  numberSize: string;
+}>(()=>{
+  switch (props.fishSize) {
 
-const props = withDefaults(defineProps<Props>(), {
-  fishType: 'laranja',
+    case 'sm':
+      return {
+        fishSize: 'w-[85px] h-[180px]',
+        numberSize: 'text-2xl w-[30px] h-[30px] bottom-[50px]'
+      }
+  
+    default:
+      return {
+        fishSize: 'w-[240px] h-[500px]',
+        numberSize: 'text-7xl w-[100px] h-[100px] bottom-[190px]'
+      }
+  }
 })
 
-const atualFishType = computed(()=>{
-  switch (props.fishType) {
-    
-    case 'laranja':
-      return '/peixelaranja.png'
+const props = withDefaults(defineProps<Fish>(), {
+  color: 'laranja',
+  fishSize: 'lg',
+})
 
-    case 'rosa':
-    return '/peixerosa.png'
+const fishImagePaths: FishColor = {
+  'amarelo': '/peixe_amarelo.png',
+  'azul': '/peixe_azul.png',
+  'ciano': '/peixe_ciano.png',
+  'laranja': '/peixe_laranja.png',
+  'lilas': '/peixe_lilas.png',
+  'rosa': '/peixe_rosa.png',
+  'salmao': '/peixe_salmao.png',
+  'verde': '/peixe_verde.png',
+}
+const atualFishType = computed(()=> fishImagePaths[props.color]);
 
-    default:
-      return '/peixerosa.png'
+// flip card 
+
+const flipContainer = ref<HTMLElement | null>(null);
+const isFlipped = ref(false);
+
+
+// Resetting The Rotation Back To Original State
+
+const resetRotation = () => {
+  if (flipContainer.value !== null) {
+    flipContainer.value.style.transform = "";
   }
+};
+
+// Checking The Value Of isFlipped & Changing its Value
+
+const handleFlip = () => {
+  if (flipContainer.value !== null) {
+    if (isFlipped.value) {
+      flipContainer.value.classList.remove("is-flipped");
+      isFlipped.value = false;
+    } else {
+      flipContainer.value.classList.add("is-flipped");
+      isFlipped.value = true;
+    }
+  }
+};
+
+watch(()=>props.isRevealed, ()=>{
+  handleFlip();  
 })
 
 </script>
 
 <style scoped>
+/* Adding Perspective To Parent Container */
 
+.flip-container {
+  perspective: 1800px;
+  transform-style: preserve-3d;
+  transition: transform 0.5s ease;
+}
+
+/* Hiding The Back Of Card */
+
+.flip-card-front,
+.flip-card-back {
+  backface-visibility: hidden;
+  transform-style: preserve-3d;
+}
+
+/* Setting The Default Postion Of The Back Of The Card */
+
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+
+/* Flipping The Card On 'Enter' Key Event */
+
+.flip-container.is-flipped .flip-card-front {
+  transform: rotateY(180deg);
+}
+
+.flip-container.is-flipped .flip-card-back {
+  transform: rotateY(360deg);
+}
 </style>
