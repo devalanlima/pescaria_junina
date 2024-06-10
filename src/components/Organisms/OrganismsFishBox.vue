b599e629661b65f783560e253734fd6ba1aa4479
Alan Lima <dev.alanlima@gmail.com>
2024-06-10 15:07:41 -0300

    refactor: move fishs array from component to store

--- src/components/Organisms/OrganismsFishBox.vue
+++ src/components/Organisms/OrganismsFishBox.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="relative">
-    <template v-for="(fish, index) in fishs" :key="index">
-      <button 
-      class="absolute translate-x-[28px] -translate-y-[154px] cursor-pointer z-40 w-[30px] h-[30px] rounded-full"
+    <template v-for="fish in storeArr.fishs" :key="fish.fishNumber">
+      <button
+      class="absolute translate-x-[28px] -translate-y-[154px] cursor-pointer z-[41] w-[30px] h-[30px] rounded-full"
       :class="[fish.coordinates?.Buried]"
       @click="fish.isRevealed = true"
       >
       </button>
-      <MoleculesFishBuried 
-      @click="fish.isRevealed ? fish.coordinates = {Buried: 'sr-only', Revealed: ''} : ''"
+      <MoleculesFishBuried
+      @click="fish.isRevealed ? fish.coordinates = {Buried: 'sr-only', Revealed: 'sr-only'} : ''"
       class="absolute w-fit transition-transform duration-1000 ease-in-out"
-      :class="[fish.coordinates?.Buried, fish.isRevealed && fish.coordinates?.Revealed]"
+      :class="fish.isRevealed ? fish.coordinates?.Revealed : fish.coordinates?.Buried"
       :color="fish.color"
-      :is-burried="!fish.isRevealed" 
+      :is-burried="!fish.isRevealed"
       :is-revealed="fish.isRevealed"
       :fish-size="fish.isRevealed ? 'lg' : 'sm'"
+      :fish-number="fish.fishNumber"
       />
-    </template>
+      </template>
     <AtomsSandBox class="w-[1000px] h-[530px]"/>
   </div>
 </template>
@@ -24,76 +25,8 @@
 <script setup lang="ts">
 import AtomsSandBox from '../Atoms/AtomsSandBox.vue';
 import MoleculesFishBuried from '../Molecules/MoleculesFishBuried.vue';
-import type { Fish } from '@/types/PescariaTypes';
-import { onMounted, ref } from "vue";
+import { useStoreArr } from "../../stores/storeArr";
 
-const fishs = ref<Array<Fish>>([
-  {
-    color: 'rosa',
-    coordinates: {
-      Buried: 'left-[140px] bottom-[295px]',
-      Revealed: 'absolute z-50 translate-x-[280px] translate-y-[-32px]'
-    },
-    isRevealed: false,
-  },
-  {
-    color: 'laranja',
-    coordinates: {
-      Buried: 'left-[520px] bottom-[355px]',
-      Revealed: 'translate-y-[25px] translate-x-[-100px] z-50'
-    },
-    isRevealed: false,
-  },
-  {
-    color: 'salmao',
-    coordinates: {
-      Buried: 'left-[300px] bottom-[180px] z-30',
-      Revealed: 'translate-x-[120px] translate-y-[-150px] z-50'
-    },
-    isRevealed: false,
-  },
-  {
-    color: 'azul',
-    coordinates: {
-      Buried: 'left-[430px] bottom-[250px] z-30',
-      Revealed: 'translate-x-[-10px] translate-y-[-78px] z-50'
-    },
-    isRevealed: false,
-  },
-  {
-    color: 'verde',
-    coordinates: {
-      Buried: 'left-[580px] bottom-[200px]',
-      Revealed: 'translate-x-[-160px] translate-y-[-128px] z-50'
-    },
-    isRevealed: false,
-  },
-  {
-    color: 'ciano',
-    coordinates: {
-      Buried: 'left-[280px] bottom-[340px]',
-      Revealed: 'translate-x-[140px] translate-y-[13px] z-50'
-    },
-    isRevealed: false,
-  },
-  {
-    color: 'amarelo',
-    coordinates: {
-      Buried: 'left-[720px] bottom-[280px]',
-      Revealed: 'translate-x-[-300px] translate-y-[-48px] z-50'
-    },
-    isRevealed: false,
-  },
-])
-
-const mounted = ref<boolean>(false);
-
-onMounted(()=>{
-  mounted.value = true
-})
+const storeArr = useStoreArr();
 
 </script>
-
-<style scoped>
-
-</style>
