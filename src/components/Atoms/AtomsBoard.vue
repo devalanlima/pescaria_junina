8d48fbe4a3ff522a00c9a3e7197b7b763e9126a9
Alan Lima <dev.alanlima@gmail.com>
2024-06-10 15:14:26 -0300

    feat: Create board component

---
+++ src/components/Atoms/AtomsBoard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="relative">
+      <div class="w-[10px] h-[1000px] bg-black absolute left-14 -z-10 bottom-10"></div>
+      <div class="w-[10px] h-[1000px] bg-black absolute right-16 -z-10 bottom-10"></div>
+      <img src="/premios.png" alt="quadro de prêmios">
+    </div>
+  </div>
+</template>
