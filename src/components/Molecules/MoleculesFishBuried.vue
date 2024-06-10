8f6e0139ed104db96d541419c31d39df9671fc13
Alan Lima <dev.alanlima@gmail.com>
2024-06-10 15:05:29 -0300

    feat: Add fishNumber prop to fish buried component

--- src/components/Molecules/MoleculesFishBuried.vue
+++ src/components/Molecules/MoleculesFishBuried.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-      <div class="relative w-fit h-fit grid place-items-center">
-        <AtomsSandPile
-        class="absolute bottom-8 z-10"
-        :class="!props.isBurried && 'hidden'"
-        />
-        <AtomsFish :fish-size="props.fishSize" :color="props.color" :is-revealed="props.isRevealed"  />
-      </div>
+  <div>
+    <div class="relative w-fit h-fit grid place-items-center">
+      <AtomsSandPile
+      class="absolute bottom-8 z-10"
+      :class="!props.isBurried && 'hidden'"
+      />
+      <AtomsFish 
+      :fish-size="props.fishSize" 
+      :color="props.color" 
+      :fish-number="props.fishNumber" 
+      :is-revealed="props.isRevealed" 
+      />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -16,7 +21,7 @@ import AtomsSandPile from '../Atoms/AtomsSandPile.vue';
 import type { Fish } from '@/types/PescariaTypes';
 
 interface Props extends Fish {
-  isBurried: boolean;
+isBurried: boolean;
 }
 
 const props = defineProps<Props>();
