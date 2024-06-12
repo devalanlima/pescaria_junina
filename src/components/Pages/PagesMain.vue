39173efd4db071b85b2fb8b31223895b86a03c2c
Alan Lima <dev.alanlima@gmail.com>
2024-06-12 18:17:17 -0300

    feat: Create loading when start

--- src/components/Pages/PagesMain.vue
+++ src/components/Pages/PagesMain.vue
@@ -6,17 +6,21 @@
     />
 
     <MoleculesPrizeBoard 
-    :key="attPrizeBoard"
+    v-if="showFishs"
     class="fixed top-10 right-10 w-[600px] z-50" 
     />
     
     <div class="fixed text-4xl text-red-500 bottom-[250px] right-28 z-50">
       Fantasia no sapo
     </div>
+
     <div class="flex relative right-[290px]">
-      <OrganismsFishBox class="mt-10 flex items-end"/>
-      <MoleculesFrogFishing class="absolute bottom-[110px] -right-[420px] z-40"/>
+      <OrganismsFishBox class="mt-10 flex items-end" v-if="showFishs"/>
+      <MoleculesFrogFishing v-show="showFishs" class="absolute bottom-[110px] -right-[420px] z-40"/>
     </div>
+
+    <AtomsSpin v-if="!showFishs" class="fixed top-1/2 left-1/2 -translate-x-full translate-y-full" />
+
   </div>
 </template>
 
@@ -25,15 +29,18 @@ import MoleculesPrizeBoard from '../Molecules/MoleculesPrizeBoard.vue';
 import AtomsStartButton from "../Atoms/AtomsStartButton.vue";
 import MoleculesFrogFishing from '../Molecules/MoleculesFrogFishing.vue';
 import OrganismsFishBox from '../Organisms/OrganismsFishBox.vue';
+import AtomsSpin from "../Atoms/AtomsSpin.vue";
 import { useStoreArr } from "../../stores/storeArr";
 import { ref } from "vue";
 
 const storeArr = useStoreArr();
 
-const attPrizeBoard = ref<number>(0);
-
+const showFishs = ref<boolean>(true);
 const randomizeAndStore = () =>{
   storeArr.resetFishs();
-  attPrizeBoard.value++
+  showFishs.value = false;
+  setTimeout(() => {
+    showFishs.value = true;
+  }, 500);
 }
 </script>
