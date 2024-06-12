eeb0ac6e55df19dff20257fc9873ee85aaad2653
Alan Lima <dev.alanlima@gmail.com>
2024-06-12 12:01:57 -0300

    feat: add count to organize reveal order

--- src/components/Organisms/OrganismsFishBox.vue
+++ src/components/Organisms/OrganismsFishBox.vue
@@ -2,23 +2,25 @@
   <div class="relative">
     <template v-for="fish in storeArr.fishs" :key="fish.fishNumber">
       <button
+      v-if="storeArr.fishs.length !== 0"
       class="absolute translate-x-[28px] -translate-y-[154px] cursor-pointer z-[41] w-[30px] h-[30px] rounded-full"
       :class="[fish.coordinates?.Buried]"
-      @click="fish.isRevealed = true"
+      @click="fish.revealed = count++, storeArr.setLocalStorage()"
       >
       </button>
       <MoleculesFishBuried
-      @click="fish.isRevealed ? fish.coordinates = {Buried: 'sr-only', Revealed: 'sr-only'} : ''"
+      v-if="storeArr.fishs.length !== 0 && fish.fishNumber !== undefined"
+      @click="fish.revealed ? fish.coordinates = {Buried: 'sr-only', Revealed: 'sr-only'} : '', storeArr.setLocalStorage()"
       class="absolute w-fit transition-transform duration-1000 ease-in-out"
-      :class="fish.isRevealed ? fish.coordinates?.Revealed : fish.coordinates?.Buried"
+      :class="fish.revealed ? fish.coordinates?.Revealed : fish.coordinates?.Buried"
       :color="fish.color"
-      :is-burried="!fish.isRevealed"
-      :is-revealed="fish.isRevealed"
-      :fish-size="fish.isRevealed ? 'lg' : 'sm'"
+      :is-burried="!fish.revealed"
+      :revealed="fish.revealed"
+      :fish-size="fish.revealed ? 'lg' : 'sm'"
       :fish-number="fish.fishNumber"
       />
-      </template>
-    <AtomsSandBox class="w-[1000px] h-[530px]"/>
+    </template>
+    <AtomsSandBox class="w-[1100px] h-[530px] -ml-7"/>
   </div>
 </template>
 
@@ -26,7 +28,12 @@
 import AtomsSandBox from '../Atoms/AtomsSandBox.vue';
 import MoleculesFishBuried from '../Molecules/MoleculesFishBuried.vue';
 import { useStoreArr } from "../../stores/storeArr";
+import { ref } from "vue";
 
 const storeArr = useStoreArr();
 
+const count = ref<number>(1);
+storeArr.fishs.map(fish => {
+  fish.revealed &&  count.value++
+})
 </script>
