adbbc76764fcc6dd2f399b9366e553ab7244aa2b
Alan Lima <dev.alanlima@gmail.com>
2024-06-12 12:07:45 -0300

    feat: Add sniglet text font to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      'sniglet': ["Sniglet", "sans-serif" , "system-ui"]
+    }
   },
   plugins: []
 }
