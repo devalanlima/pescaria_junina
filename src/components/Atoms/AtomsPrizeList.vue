3be92d5d95ead30e000084e9ebcd6bce877b5d5d
Alan Lima <dev.alanlima@gmail.com>
2024-06-13 11:50:12 -0300

    feat: Create prize list component

---
+++ src/components/Atoms/AtomsPrizeList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul class="flex flex-col gap-1 rounded-2xl font-sniglet z-50">
+      <template v-for="(prize, index) in storeArr.prizes" :key="prize.prizeNumber" >
+        <li 
+        :class="prize.revealed ? 'text-red-700 underline underline-offset-1' : 'text-black'"
+        >
+          {{ index + 1 }} - {{ prize.title }}
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useStoreArr } from "../../stores/storeArr";
+
+const storeArr = useStoreArr();
+
+</script>
