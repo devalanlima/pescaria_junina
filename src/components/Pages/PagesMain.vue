25e8907e59da0d39626cb2ea1171ee2c74b83ad7
Alan Lima <dev.alanlima@gmail.com>
2024-06-13 11:51:20 -0300

    feat: add fanart and prizelist

--- src/components/Pages/PagesMain.vue
+++ src/components/Pages/PagesMain.vue
@@ -10,16 +10,18 @@
     class="fixed top-10 right-10 w-[600px] z-50" 
     />
     
-    <div class="fixed text-4xl text-red-500 bottom-[250px] right-28 z-50">
-      Fantasia no sapo
-    </div>
 
     <div class="flex relative right-[290px]">
       <OrganismsFishBox class="mt-10 flex items-end" v-if="showFishs"/>
-      <MoleculesFrogFishing v-show="showFishs" class="absolute bottom-[110px] -right-[420px] z-40"/>
+      <MoleculesFrogFishing v-show="showFishs" class="absolute bottom-[100px] -right-[350px] z-40"/>
     </div>
 
     <AtomsSpin v-if="!showFishs" class="fixed top-1/2 left-1/2 -translate-x-full translate-y-full" />
+
+    <div class="fixed bottom-5 right-5 flex items-end gap-1 z-[999]">
+      <MoleculesFanart/>
+      <MoleculesPrizeList/>
+    </div>
 
   </div>
 </template>
@@ -32,6 +34,8 @@ import OrganismsFishBox from '../Organisms/OrganismsFishBox.vue';
 import AtomsSpin from "../Atoms/AtomsSpin.vue";
 import { useStoreArr } from "../../stores/storeArr";
 import { ref } from "vue";
+import MoleculesPrizeList from '../Molecules/MoleculesPrizeList.vue';
+import MoleculesFanart from '../Molecules/MoleculesFanart.vue';
 
 const storeArr = useStoreArr();
 
